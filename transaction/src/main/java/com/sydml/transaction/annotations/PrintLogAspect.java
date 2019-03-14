71251fd92574a07fecfd8aae4936d3fd3d159332
Liuym <liuym@startimes.com.cn>
2019-03-14 16:25:57 +0800

    增加注解 @PrintLog 的健壮性，注解使用失败不影响业务流程

--- transaction/src/main/java/com/sydml/transaction/annotations/PrintLogAspect.java
+++ transaction/src/main/java/com/sydml/transaction/annotations/PrintLogAspect.java
@@ -25,51 +25,68 @@ public class PrintLogAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PrintLogAspect.class);
     private static final String SPLIT = ":";
-    private static final String BLANK = " ";
     private static final String POINT = ".";
+    private static final String MARKS = "\"";
+    private static final String COMMA = ",";
+    private static final String PARAM_IS = "param.is";
+    private static final String RESULT_IS = "result.is";
+    private static final String RESULT = "result";
+    private static final String TYPE = "TYPE";
 
 
     @Pointcut("@annotation(com.sydml.transaction.annotations.PrintLog)")
-    public void controllerAspect() {
-
-    }
+    public void controllerAspect() {}
 
     @Before("controllerAspect()")
     public void doBefore(JoinPoint joinPoint) {
         StringBuilder logContent = new StringBuilder();
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        Method method = signature.getMethod();
-        Object[] args = joinPoint.getArgs();
-        Object target = joinPoint.getTarget();
-        String className = target.getClass().getSimpleName();
-        Parameter[] parameters = method.getParameters();
-        logContent.append(className + POINT + method.getName() + POINT + "param.is" + SPLIT);
-        for (int i = 0; i < parameters.length; i++) {
-            if (parameters[i].getType().isPrimitive() || isWrapClass(parameters[i].getType())) {
-                logContent.append("\"" + parameters[i].getName() + "\"" + SPLIT + "\"" + args[i] + "\"" + ",");
-            } else {
-                logContent.append(JsonUtil.encodeJson(args[i]));
+        try {
+            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+            Method method = signature.getMethod();
+            Object[] args = joinPoint.getArgs();
+            Object target = joinPoint.getTarget();
+            String className = target.getClass().getSimpleName();
+            Parameter[] parameters = method.getParameters();
+            logContent.append(className + POINT + method.getName() + POINT + PARAM_IS + SPLIT);
+            if (args.length == 0) {
+                return;
             }
+            for (int i = 0; i < parameters.length; i++) {
+                if (parameters[i].getType().isPrimitive() || isWrapClass(parameters[i].getType())) {
+                    logContent.append(MARKS + parameters[i].getName() + MARKS + SPLIT + MARKS + args[i] + MARKS + COMMA);
+                } else {
+                    logContent.append(JsonUtil.encodeJson(args[i]));
+                }
+            }
+            String content = logContent.toString();
+            logProcessor(PrintLog.REQUEST, method, content);
+        } catch (Exception e) {
+            LOGGER.warn("PrintLogAspect.processor.request.log.print.error");
         }
-        String content = logContent.toString();
-        logProcessor(PrintLog.REQUEST, method, content);
     }
 
 
-    @AfterReturning(returning = "result", value = "controllerAspect()")
+    @AfterReturning(returning = RESULT, value = "controllerAspect()")
     public void doAfter(JoinPoint joinPoint, Object result) {
-        StringBuilder logContent = new StringBuilder();
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        Method method = signature.getMethod();
-        Object target = joinPoint.getTarget();
-        String className = target.getClass().getSimpleName();
-        String methodName = method.getName();
-        logContent.append(className + POINT + methodName + POINT + "result.is" + SPLIT);
-        if (result != null) {
-            logContent.append(JsonUtil.encodeJson(result));
+        if (result == null) {
+            return;
         }
-        String content = logContent.toString();
-        logProcessor(PrintLog.RESPONSE, method, content);
+        StringBuilder logContent = new StringBuilder();
+        try {
+            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+            Method method = signature.getMethod();
+            Object target = joinPoint.getTarget();
+            String className = target.getClass().getSimpleName();
+            String methodName = method.getName();
+            logContent.append(className + POINT + methodName + POINT + RESULT_IS + SPLIT);
+            if (result != null) {
+                logContent.append(JsonUtil.encodeJson(result));
+            }
+            String content = logContent.toString();
+            logProcessor(PrintLog.RESPONSE, method, content);
+        } catch (Exception e) {
+            LOGGER.warn("PrintLogAspect.processor.return.log.print.error");
+        }
 
     }
 
@@ -103,7 +120,7 @@ public class PrintLogAspect {
 
     public static boolean isWrapClass(Class clz) {
         try {
-            return ((Class) clz.getField("TYPE").get(null)).isPrimitive();
+            return ((Class) clz.getField(TYPE).get(null)).isPrimitive();
         } catch (Exception e) {
             return false;
         }
