99a3f3d61682acd9348b516f1a42646af3ae3567
Liuym <liuym@startimes.com.cn>
2019-03-25 16:08:11 +0800

    Authorization 项目SQL

--- authorization/src/main/resources/sql/init.sql
+++ authorization/src/main/resources/sql/init.sql
@@ -2,6 +2,8 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `username` varchar(30) DEFAULT NULL COMMENT '用户名',
   `password` varchar(30) DEFAULT NULL COMMENT '密码',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
   `status` tinyint(1) DEFAULT NULL COMMENT '删除标识 0:未删除 1:已删除',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
