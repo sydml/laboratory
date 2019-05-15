d0a7e6489a85ca840170144d3bdcecc9e29c0bc9
Liuym <liuym@startimes.com.cn>
2019-05-15 11:27:34 +0800

    搭建zipkin

--- zipkin/src/main/java/com/sydml/zipkin/ZipkinApplication.java
+++ zipkin/src/main/java/com/sydml/zipkin/ZipkinApplication.java
@@ -2,10 +2,12 @@ package com.sydml.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
+@EnableEurekaClient
 public class ZipkinApplication {
 
     public static void main(String[] args) {
